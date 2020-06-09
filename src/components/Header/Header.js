0e2355b5b42cde1fa07a6aa596710c7d99e71912
Thiago Emmerich <dev2@brametec.com.br>
2020-06-09 14:16:11 -0300

    Created dashboard screen

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Header.css';
 
+import logo from '../../images/logo.jpeg';
+
 const Header = () => (
-    <header id='main-header'>Product Manager</header>
+    <div className='main-header'>
+        <div >
+            <img src={logo} alt="Avatar" className="avatar" />
+        </div>
+        <button className='logout-button'>Sair</button>
+    </div>
 );
 
 export default Header;
