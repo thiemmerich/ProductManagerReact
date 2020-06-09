7ecee0eae4ecb3fd18ceab3a8d214278feb6b4f8
Thiago Emmerich <dev2@brametec.com.br>
2020-06-09 13:36:32 -0300

    Added storage for token and modal for cadastro class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 
 import Header from './components/Header/Header';
 import Main from './pages/main/main';
 import Login from './pages/login/login';
 import Cadastro from './components/Cadastro/Cadastro';
+import Modal from './components/Cadastro/Modal';
 
-function App() {
-  /*return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
-  );*/
+export default class App extends Component {
 
-  return (
-    <div className="App">
-      <Header />
-      <Cadastro />
-    </div>
-  );
+  state = {
+    show: false
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  }
+
+  hideModal = () => {
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Modal show={this.state.show} handleClose={this.hideModal} >
+          <Cadastro />
+        </Modal>
+        <button type='button' onClick={this.showModal}>Open</button>
+        <Main/>
+      </div>
+    );
+  }
 }
 
-export default App;
+//export default App;
